5f4e95149e68424b51a1261d4b18cd92115195eb
prabhashrai02 <prabhashsahaj@gmail.com>
2021-12-29 16:15:48 +0530

    init

---
+++ src/Components/Loginpage/Loginpage.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+
+class Loginpage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  
+            <div>
+                <Grid container>
+                    <Grid item xs={3}>
+                    </Grid>
+                    <Grid item xs={6}>
+                    </Grid>
+                    <Grid item xs={3}>
+                    </Grid>
+                </Grid>
+            </div>
+        );
+    }
+}
+ 
+export default Loginpage;
